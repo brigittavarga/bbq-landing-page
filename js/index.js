40a293855b2c9445ecbde7b669d4e57b1a01b8e8
Brigitta Varga <v.brigitta.zs@gmail.com>
2021-04-06 16:33:17 +0200

    updated htmlContent

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
+// store the form in a variable
 let emailCollectorForm = document.querySelector("#Email-Collector")
 
+// add an event listener to the submit button INSIDE of the form 
 emailCollectorForm.addEventListener("submit", event => {
+    // stop the default event behavior
     event.preventDefault()
 
     let ourFormData = new FormData(event.target)
 
+    // store input name="firstName"
     let userFirstName = ourFormData.get("firstName")
+
+    // store input name="emailAddress"
+    let userEmailAddress = ourFormData.get("emailAddress")
 
     let updatedHTMLContent = `
             <h2>Congratulations, ${userFirstName}!</h2>
             <p>You‘re on your way to becoming a BBQ Master!</p>
-            <p class="fine-print">We'll never share your information without your permission</p>
+            <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmailAddress}</p>
     `
 
     let ourMainContent = document.querySelector("#Main-Content")
